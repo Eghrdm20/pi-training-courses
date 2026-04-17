0947cb8eac8654c13eea039f68a3913f4618795c
Eghrdm20 <100941270+Eghrdm20@users.noreply.github.com>
2026-04-17 15:45:21 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 import type { Metadata } from "next"
 import type { ReactNode } from "react"
+import Script from "next/script"
 
 export const metadata: Metadata = {
   title: "pi-training-courses",
@@ -14,7 +15,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ar" dir="rtl">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script
+          src="https://sdk.minepi.com/pi-sdk.js"
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   )
 }
